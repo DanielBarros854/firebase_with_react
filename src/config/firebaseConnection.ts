ca1a4538b1d8e79d8487a9b73c59acd3f6237a6f
DanielBarros854 <91908602+DanielBarros854@users.noreply.github.com>
2022-05-04 13:26:28 -0300

    feat: organize firebase folder

--- src/config/firebaseConnection.ts
+++ src/config/firebaseConnection.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore'
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
@@ -12,6 +13,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export default db;
+export const db = getFirestore(app);
+export const authentication = getAuth(app);
